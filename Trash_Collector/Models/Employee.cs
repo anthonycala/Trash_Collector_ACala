9c7292c4c2e1a8cbd3733d637c616a0574170cc7
unknown <antcala@icloud.com>
2019-10-28 15:08:12 -0500

    fixed Employee.cs and Customer.cs and DbSet Customer and DbSet Employee

--- Trash_Collector/Models/Employee.cs
+++ Trash_Collector/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,13 @@ namespace Trash_Collector.Models
 {
     public class Employee
     {
+        [Key]
+        public int Id { get; set; }
+        public int zipCode { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+
     }
 }
+
--- Trash_Collector/Models/IdentityModels.cs
+++ Trash_Collector/Models/IdentityModels.cs
@@ -9,6 +9,8 @@ namespace Trash_Collector.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType

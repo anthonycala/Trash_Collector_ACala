df5084420ba505e524e5919cd4dc7c0a34931af6
unknown <antcala@icloud.com>
2019-10-29 16:16:27 -0500

    updated the AccountViewMode.cs added UserRoles, and UserNames to the RegisterViewModel

--- Trash_Collector/Models/AccountViewModels.cs
+++ Trash_Collector/Models/AccountViewModels.cs
@@ -65,10 +65,19 @@ namespace Trash_Collector.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "UserRoles")]
+        public string UserRoles { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [Required]
+        [EmailAddress]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+        
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
